3f2a30ef627766c32e31fd696c01a8931796959a
Cruz-Andre <cruzandrecal@gmail.com>
2022-06-22 21:36:54 -0300

    22-06-22 Dispondo elementos com Flexbox e Grid

---
+++ Alura/FlexeGrid/index.js
@@ -0,0 +1,8 @@
+// mostrar e recolher menu
+
+const botaoMenu = document.querySelector('.cabecalho__menu');
+const menu = document.querySelector('.menu-lateral');
+
+botaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral--ativo');
+})
