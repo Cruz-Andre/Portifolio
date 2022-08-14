8cb9ac22e8901ab27fb0c459c405cefe6a9f0c5a
Cruz-Andre <cruzandrecal@gmail.com>
2022-08-14 19:38:07 -0300

    Rodapé responsivo

--- Alura/REACT/DesenvolvendoComJavaScript/organo/src/componentes/Rodape/Rodape.js
+++ Alura/REACT/DesenvolvendoComJavaScript/organo/src/componentes/Rodape/Rodape.js
@@ -3,17 +3,35 @@ import './Rodape.css'
 const Rodape = () => {
     return (
         <footer className='rodapepe'>
-            <a href='https://facebook.com' target='_blank' rel="noreferrer">
-                <img src="/imagens/fb.png" alt='Icone Facebook'/> 
-            </a>
-            <a href='https://twitter.com' target='_blank' rel="noreferrer">
-                <img src="/imagens/tw.png" alt='Icone Twitter'/>
-            </a>
-            <a href='https://instagram.com' target='_blank' rel="noreferrer">
-                <img src="/imagens/ig.png" alt='Icone Instagram'/>
-            </a>
-            <img src="/imagens/logo.png" alt="Logo Organo" />
-            <p>Desenvolvido por Alura</p>
+            <section>
+                <ul>
+                    <li>
+                        <a href='https://facebook.com' target='_blank' rel="noreferrer">
+                            <img src="/imagens/fb.png" alt='Icone Facebook'/> 
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href='https://twitter.com' target='_blank' rel="noreferrer">
+                            <img src="/imagens/tw.png" alt='Icone Twitter'/>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href='https://instagram.com' target='_blank' rel="noreferrer">
+                            <img src="/imagens/ig.png" alt='Icone Instagram'/>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+
+            <section>
+                <img src="/imagens/logo.png" alt="Logo Organo" />
+            </section>
+
+            <section>
+                <p>Desenvolvido por Alura</p>
+            </section>
         </footer>
     )
 }
